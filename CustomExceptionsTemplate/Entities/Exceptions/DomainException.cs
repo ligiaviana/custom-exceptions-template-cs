0ab655bb188beac9e7a008287280dee31a41ceca
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-08-08 10:44:42 -0300

    custom-exceptions-template

---
+++ CustomExceptionsTemplate/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomExceptionsTemplate.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        { 
+        }
+    }
+}
